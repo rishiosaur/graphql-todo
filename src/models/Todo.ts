c7f4347992e1b6e1c627de5b0f9b7962a70c4c89
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-31 19:41:54 -0500

    add in decorators

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,11 +1,16 @@
-import { Field, ObjectType } from 'type-graphql'
+import { Field, ObjectType, ID } from 'type-graphql'
 
+@ObjectType()
 export default class Todo {
+	@Field(() => ID)
 	id: string
 
+	@Field(() => Boolean)
 	completed: boolean
 
+	@Field(() => String)
 	title: string
 
+	@Field(() => String)
 	description: string
 }

5453ed345c6e3cd77b7c287cf3c8c604190b0dd0
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-31 20:30:33 -0500

    add test data

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,28 @@
 import { Arg, Query, Resolver } from 'type-graphql'
 import { ITodo } from '../models/Todo'
 import Todo from '../models/Todo'
-const data: ITodo[] = []
+const data: ITodo[] = [
+	{
+		id: '2815a531-66c7-4274-ac83-c6b45abe8885',
+		completed: true,
+		title: 'culpa assumenda voluptas',
+		description: 'Velit culpa quasi temporibus rerum.',
+	},
+	{
+		id: 'd89101c1-9bc6-43ff-a1bf-b4bf7d018a5c',
+		completed: false,
+		title:
+			'Facilis quis optio recusandae laboriosam architecto aut iusto debitis eaque. Consectetur ducimus aut asperiores tempora aut ea.',
+		description:
+			'Dolore eaque quos dolorem in.\nPlaceat consectetur quo distinctio.',
+	},
+	{
+		id: '09f4332a-f037-4504-94b9-053b5f5da42b',
+		completed: true,
+		title: 'voluptas architecto et',
+		description: 'rerum',
+	},
+]
 
 @Resolver()
 export default class TodoQueryResolver {

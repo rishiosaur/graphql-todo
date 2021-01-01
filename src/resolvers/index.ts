29c916ab2f9ef52d965f5140fb977f79ba5d7c04
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-31 20:27:47 -0500

    add in single todo resolver w/ arg parameter

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from 'type-graphql'
+import { Arg, Query, Resolver } from 'type-graphql'
 import { ITodo } from '../models/Todo'
 import Todo from '../models/Todo'
 const data: ITodo[] = []
@@ -8,5 +8,16 @@ export default class TodoQueryResolver {
 	@Query(() => [Todo])
 	public todos() {
 		return data.map((todo) => new Todo(todo))
+	}
+
+	@Query(() => Todo)
+	public todo(@Arg('id') id: string) {
+		const todo = data.filter((todo) => todo.id === id)[0]
+
+		if (todo) {
+			return new Todo(todo)
+		} else {
+			throw new Error(`Could not find Todo with ID ${id}`)
+		}
 	}
 }

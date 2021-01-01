0d1c33ed129d33849cb2765454d6175e025212f9
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-31 20:12:49 -0500

    add interface todo

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,7 +1,18 @@
 import { Field, ObjectType, ID } from 'type-graphql'
 
+export interface ITodo {
+	id: string
+	completed: boolean
+	title: string
+	description: string
+}
+
 @ObjectType()
-export default class Todo {
+export default class Todo implements ITodo {
+	constructor(todo: ITodo) {
+		Object.assign(this, todo)
+	}
+
 	@Field(() => ID)
 	id: string
 
